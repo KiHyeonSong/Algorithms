d136da2cfc77d8b55e2fc339060ba5a3821dd8cf
KiHyeonSong <rlgusl8642@naver.com>
2019-11-12 22:16:50 +0900

    Boubble_Sort

---
+++ Bubble_Sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define SWAP(X,Y,Z) ((Z) = (X) , (X) = (Y), (Y)=(Z))
+
+int main(void) {
+	int ary[10] = { 1,10,5,8,7,6,4,3,2,9 };
+	int i, j, temp, index;
+
+	printf("버블정렬 전\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", ary[i]);
+	}
+	printf("\n");
+
+	for (j = 9; j > 0; j--)
+	{
+		for (i = 0; i < j; i++)
+		{
+			if (ary[i] > ary[i + 1]) {
+				SWAP(ary[i], ary[i + 1], temp);
+			}
+		}
+
+	}
+	printf("버블정렬 후\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", ary[i]);
+	}
+
+	return 0;
+}
